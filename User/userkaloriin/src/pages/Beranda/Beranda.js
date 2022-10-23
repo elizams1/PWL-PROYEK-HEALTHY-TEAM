099924e859dcb14240dfe80112c62ca530684899
elizamaharani <elizamaharani@gmail.com>
2022-10-23 20:18:38 +0700

    beranda page not at all

--- User/userkaloriin/src/pages/Beranda/Beranda.js
+++ User/userkaloriin/src/pages/Beranda/Beranda.js
@@ -1,9 +1,29 @@
 import React from "react";
-
+import { 
+  Button,
+  Input } from '@chakra-ui/react';
+import './Beranda.css'; 
+import {BsSearch} from 'react-icons/bs';
 function Beranda(){
 
   return(
-    <p>ini beranda</p>
+    <div className="content">
+      <div className="beranda">
+        <div className="top-beranda">
+          <p className="type-name-page">Beranda</p>
+          <div className="search-fitur">
+            <Input fontSize="xs" placeholder="Cari Makanan Minuman" focusBorderColor='darkgreen' />
+            {/* value={value} onChange={} */}
+              <Button size='md' color='#155D27' ><BsSearch/></Button>
+            {/* onClick={()=> getNewsList(null,value)}><BsSearch/ */}
+          </div>
+        </div>
+        <div className="catalog-beranda">
+          
+        </div>
+      </div>
+      
+    </div>
   );
 }
 
