a016be61ea84b69c8de3dd9651073d17d524f30e
elizamaharani <elizamaharani@gmail.com>
2022-11-05 22:59:13 +0700

    footer

--- User/userkaloriin/src/App.js
+++ User/userkaloriin/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Routes, Route} from "react-router-dom";
 import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
 import Beranda from './pages/Beranda/Beranda.js';
 import Riwayat from './pages/Riwayat/Riwayat.js';
 import Postingan from './pages/Postingan/Postingan.js';
@@ -21,6 +22,7 @@ function App() {
         <Route path="/profil" element={<Profil/>}></Route>
         <Route path="/tambah-postingan" element={<TambahPostingan/>}></Route>
       </Routes>
+      <Footer/>
     </ChakraProvider>
   );
 }
---
+++ User/userkaloriin/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css';
+
+function Postingan(){
+
+  return(
+    <div className="footer">
+      <div className="footer-item">
+        <p className="text-footer">Dibuat oleh HealthyTeam</p>
+      </div>
+    </div>
+  );
+}
+
+export default Postingan;
