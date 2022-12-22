22ce733de6d22a28cc6e391f0d1a478921fd4456
elizamaharani <elizamaharani@gmail.com>
2022-12-22 08:47:27 +0700

    new image

--- User/userkaloriin/src/pages/Login/Login.js
+++ User/userkaloriin/src/pages/Login/Login.js
@@ -1,26 +1,54 @@
-import React ,{ useState }from "react";
+import React, {useState, useEffect} from "react";
 import { useNavigate,  } from "react-router-dom";
 import { useAuth } from '../../components/auth.js';
 import './Login.css';
+import axios from 'axios';
 
 function Postingan(){
 
   const [user, setUser]=useState('');
   const [password, setPassword]=useState('');
+  const [PenggunaData,setPenggunaData] =useState([]);
+  const [userSearch, setUserearch] = useState(PenggunaData);
   const auth = useAuth();
   const navigate = useNavigate();
   //const location = useLocation();
   //const from = location.state?.pathname || "/";
 
+   useEffect(()=> {
+    axios
+      .get("http://localhost:3306/user")
+      .then(function (user){
+          setPenggunaData(user.data.data);
+          console.log(user.data.data)
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    },[]
+  );
 
   const handleLogin = () =>{
     auth.login(user)
-    if(user==="user"){
-      navigate('/user/beranda')
-    console.log(user)
-    }else{
-      navigate('/admin/beranda')
+    
+    const query = user;
+    var theUser = [...PenggunaData]
+
+    theUser = theUser.filter((item) => {
+      return item.email.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+
+    setUserearch(theUser);
+
+    for(let i=0; i<userSearch.length;i++){
+      if(userSearch[i].role==='user'){
+        navigate('/user/beranda')
+      }
+      else if(userSearch[i].role==='admin'){
+        navigate('/admin/beranda')
+      }
     }
+    console.log(user)
     console.log(password)
    
   }
